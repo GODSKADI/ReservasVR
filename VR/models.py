c5a4ac11d02a5360028929c37104f5621f776b27
godskadi <aidrean.gf@gmail.com>
2019-10-28 15:44:09 +0100

    site admin

---
+++ VR/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Curso(models.Model):
+	nombre = models.CharField(max_length = 200)
+	clase = models.CharField(max_length = 200)
+
+class Aula(models.Model):
+	nombre = models.CharField(max_length = 200)
+	num_gafas = models.IntegerField(default = 0)
+	aforo = models.IntegerField(default = 0)
+
+class Reserva(models.Model):
+	nombre = models.ForeignKey(User, on_delete = models.CASCADE)
+	num_personas = models.IntegerField(default = 0)
+	num_gafas = models.IntegerField(default = 0)
+	aula = models.ForeignKey(Aula, on_delete = models.CASCADE)
+	fecha_hora = models.DateTimeField(default = timezone.now) 
+	incio_hora = models.DateTimeField(default = timezone.now)
+	final_hora = models.DateTimeField(default = timezone.now)
+
+class Gafa(models.Model):
+	nombre = models.CharField(max_length = 200)
+	aula = models.ForeignKey(Aula, on_delete = models.CASCADE)
